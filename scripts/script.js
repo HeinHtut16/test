922c434d05ef242fa01cc1c131c2d9d7c6239fc5
Hein Htut Aung <heinhtutaung@Heins-iMac.local>
2025-03-06 23:42:50 +0630

    Finish

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,51 @@
-let bigImage = document.getElementById("big-image");
-let srcGiftCard = "Gift Card ";
-let image = 1;
-let dots = document.querySelectorAll(".dot");
+document.addEventListener("DOMContentLoaded", () => {
+    let bigImage = document.getElementById("big-image");
+    let srcGiftCard = "Gift Card ";
+    let image = 1;
+    let dots = document.querySelectorAll(".dot");
 
-// Preload images
-let images = [];
-for (let i = 1; i <= 6; i++) {
-    images[i] = new Image();
-    images[i].src = `../images/${srcGiftCard}${i}.jpg`;
-}
-
-document.getElementById("prev").addEventListener("click", () => {
-    if(image < 2) image = 7;
-    bigImage.setAttribute("src", `../images/${srcGiftCard}${image-1}.jpg`);
-    image--;
-    for(let i = 0; i < dots.length; i++) {
-        if(dots[i].classList.contains("active")) {
-            dots[i].classList.remove("active");
-        }
+    // Preload images
+    let images = [];
+    for (let i = 1; i <= 6; i++) {
+        images[i] = new Image();
+        images[i].src = `../images/${srcGiftCard}${i}.jpg`;
     }
-    dots[image-1].classList.add("active");
-});
 
-document.getElementById("next").addEventListener("click", () => {
-    if(image > 5) image = 0;
-    bigImage.setAttribute("src", `../images/${srcGiftCard}${image+1}.jpg`);
-    image++;
-    for(let i = 0; i < dots.length; i++) {
-        if(dots[i].classList.contains("active")) {
-            dots[i].classList.remove("active");
-        }
-    }
-    dots[image-1].classList.add("active");
-});
-
-document.addEventListener("click", (event) => {
-    for(let i = 0; i < dots.length; i++) {
-        if(event.target.matches(".dot" + (i+1))) {
-            for(let i = 0; i < dots.length; i++) {
-                if(dots[i].classList.contains("active")) {
-                    dots[i].classList.remove("active");
-                }
+    document.getElementById("prev").addEventListener("click", () => {
+        if(image < 2) image = 7;
+        bigImage.setAttribute("src", `../images/${srcGiftCard}${image-1}.jpg`);
+        image--;
+        for(let i = 0; i < dots.length; i++) {
+            if(dots[i].classList.contains("active")) {
+                dots[i].classList.remove("active");
             }
-            dots[i].classList.add("active");
-            bigImage.setAttribute("src", `../images/${srcGiftCard}${i+1}.jpg`);
         }
-    }
+        dots[image-1].classList.add("active");
+    });
+
+    document.getElementById("next").addEventListener("click", () => {
+        if(image > 5) image = 0;
+        bigImage.setAttribute("src", `../images/${srcGiftCard}${image+1}.jpg`);
+        image++;
+        for(let i = 0; i < dots.length; i++) {
+            if(dots[i].classList.contains("active")) {
+                dots[i].classList.remove("active");
+            }
+        }
+        dots[image-1].classList.add("active");
+    });
+
+    document.addEventListener("click", (event) => {
+        for(let i = 0; i < dots.length; i++) {
+            if(event.target.matches(".dot" + (i+1))) {
+                for(let i = 0; i < dots.length; i++) {
+                    if(dots[i].classList.contains("active")) {
+                        dots[i].classList.remove("active");
+                    }
+                }
+                dots[i].classList.add("active");
+                bigImage.setAttribute("src", `../images/${srcGiftCard}${i+1}.jpg`);
+            }
+        }
+    });
 });
