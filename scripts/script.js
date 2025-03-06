76b62317a37ffc70d039719e2533b44919a07192
Hein Htut Aung <heinhtutaung@Heins-iMac.local>
2025-03-06 22:30:26 +0630

    Finish

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,41 @@
-const carouselImage = document.getElementById("carousel-image");
-const srcOil = "Gift Card ";
-const dots = document.querySelectorAll(".dot");
-const carouselTitle = document.querySelector(".carousel-text-container h2");
-const carouselText = document.querySelector(".carousel-text-container p");
-let autoSlide = setInterval(next, 2500); 
+let bigImage = document.getElementById("big-image");
+let srcGiftCard = "Gift Card ";
 let image = 1;
+let dots = document.querySelectorAll(".dot");
+
+document.addEventListener("click", (event) => {
+    for(let i = 0; i < smallImages.length; i++)
+    {
+        if(event.target.matches(".small-image" + (i+1)))
+        {
+            bigImage.setAttribute("src", "../images/"+srcGiftCard+(i+1)+".jpg");
+            console.log(bigImage);
+        }
+    }
+});
+
+document.addEventListener("click", (event) => {
+    for(let i = 0; i < denominations.length; i++)
+    {
+        if(event.target.matches(".denomination" + (i+1)))
+        {
+            for(let i = 0; i < denominations.length; i++)
+            {
+                if(denominations[i].classList.contains("active"))
+                {
+                    denominations[i].classList.remove("active");
+                }
+            }
+            denominations[i].classList.add("active");
+            price.textContent = denominations[i].textContent;
+        }
+    }
+});
 
 document.getElementById("prev").addEventListener("click", () => {
-    prev();
-    resetAutoSlide();
-});
-document.getElementById("next").addEventListener("click", () => {
-    next();
-    resetAutoSlide();
-});
-
-function prev() {
-    if(image < 2) image = 5;
+    if(image < 2) image = 7;
+    bigImage.setAttribute("src", "../images/"+srcGiftCard+(image-1)+".jpg");
     image--;
-    // checkCarouselTitleAndText();
-    carouselImage.setAttribute("src", `images/${srcOil}${image}.jpg`);
     for(let i = 0; i < dots.length; i++)
     {
         if(dots[i].classList.contains("active"))
@@ -28,13 +44,12 @@ function prev() {
         }
     }
     dots[image-1].classList.add("active");
-}
+});
 
-function next() {
-    if(image > 3) image = 0;
+document.getElementById("next").addEventListener("click", () => {
+    if(image > 5) image = 0;
+    bigImage.setAttribute("src", "../images/"+srcGiftCard+(image+1)+".jpg");
     image++;
-    // checkCarouselTitleAndText();
-    carouselImage.setAttribute("src", `images/${srcOil}${image}.jpg`);
     for(let i = 0; i < dots.length; i++)
     {
         if(dots[i].classList.contains("active"))
@@ -43,16 +58,7 @@ function next() {
         }
     }
     dots[image-1].classList.add("active");
-}
-
-function startAutoSlide() {
-    autoSlide = setInterval(next, 2500);
-}
-
-function resetAutoSlide() {
-    clearInterval(autoSlide);
-    startAutoSlide();
-}
+});
 
 
 document.addEventListener("click", (event) => {
@@ -68,26 +74,7 @@ document.addEventListener("click", (event) => {
                 }
             }
             dots[i].classList.add("active");
-            image = i + 1;
-            // checkCarouselTitleAndText();
-            // resetAutoSlide();
-            carouselImage.setAttribute("src", `images/${srcOil}${image}.jpg`);
+            bigImage.setAttribute("src", "../images/"+srcGiftCard+(i+1)+".jpg");
         }
     }
 });
-
-// function checkCarouselTitleAndText() {
-//     if(image === 1) {
-//         carouselTitle.textContent = "Petrol Engine Oil";
-//         carouselText.textContent = "Lubricant for your car motor engines";
-//     } else if (image === 2) {
-//         carouselTitle.textContent = "Motorcycle Oil";
-//         carouselText.textContent = "Lubricant for your motorcycle";
-//     } else if (image === 3) {
-//         carouselTitle.textContent = "Commercial Oil";
-//         carouselText.textContent = "Lubricant for your commercial vehicles";
-//     } else if (image === 4) {
-//         carouselTitle.textContent = "Industrial Oil";
-//         carouselText.textContent = "Lubricant for general industrial application";
-//     }
-// }
