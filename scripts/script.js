0007eb6a7900b7887571ab79e9ce80531d26c167
Hein Htut Aung <heinhtutaung@Heins-iMac.local>
2025-03-06 23:04:31 +0630

    Finish

--- scripts/script.js
+++ scripts/script.js
@@ -3,26 +3,26 @@ let srcGiftCard = "Gift Card ";
 let image = 1;
 let dots = document.querySelectorAll(".dot");
 
+// Preload images
+let images = [];
+for (let i = 1; i <= 6; i++) {
+    images[i] = new Image();
+    images[i].src = `../images/${srcGiftCard}${i}.jpg`;
+}
+
 document.addEventListener("click", (event) => {
-    for(let i = 0; i < smallImages.length; i++)
-    {
-        if(event.target.matches(".small-image" + (i+1)))
-        {
-            bigImage.setAttribute("src", "../images/"+srcGiftCard+(i+1)+".jpg");
-            console.log(bigImage);
+    for(let i = 0; i < smallImages.length; i++) {
+        if(event.target.matches(".small-image" + (i+1))) {
+            bigImage.setAttribute("src", `../images/${srcGiftCard}${i+1}.jpg`);
         }
     }
 });
 
 document.addEventListener("click", (event) => {
-    for(let i = 0; i < denominations.length; i++)
-    {
-        if(event.target.matches(".denomination" + (i+1)))
-        {
-            for(let i = 0; i < denominations.length; i++)
-            {
-                if(denominations[i].classList.contains("active"))
-                {
+    for(let i = 0; i < denominations.length; i++) {
+        if(event.target.matches(".denomination" + (i+1))) {
+            for(let i = 0; i < denominations.length; i++) {
+                if(denominations[i].classList.contains("active")) {
                     denominations[i].classList.remove("active");
                 }
             }
@@ -34,12 +34,10 @@ document.addEventListener("click", (event) => {
 
 document.getElementById("prev").addEventListener("click", () => {
     if(image < 2) image = 7;
-    bigImage.setAttribute("src", "../images/"+srcGiftCard+(image-1)+".jpg");
+    bigImage.setAttribute("src", `../images/${srcGiftCard}${image-1}.jpg`);
     image--;
-    for(let i = 0; i < dots.length; i++)
-    {
-        if(dots[i].classList.contains("active"))
-        {
+    for(let i = 0; i < dots.length; i++) {
+        if(dots[i].classList.contains("active")) {
             dots[i].classList.remove("active");
         }
     }
@@ -48,33 +46,26 @@ document.getElementById("prev").addEventListener("click", () => {
 
 document.getElementById("next").addEventListener("click", () => {
     if(image > 5) image = 0;
-    bigImage.setAttribute("src", "../images/"+srcGiftCard+(image+1)+".jpg");
+    bigImage.setAttribute("src", `../images/${srcGiftCard}${image+1}.jpg`);
     image++;
-    for(let i = 0; i < dots.length; i++)
-    {
-        if(dots[i].classList.contains("active"))
-        {
+    for(let i = 0; i < dots.length; i++) {
+        if(dots[i].classList.contains("active")) {
             dots[i].classList.remove("active");
         }
     }
     dots[image-1].classList.add("active");
 });
 
-
 document.addEventListener("click", (event) => {
-    for(let i = 0; i < dots.length; i++)
-    {
-        if(event.target.matches(".dot" + (i+1)))
-        {
-            for(let i = 0; i < dots.length; i++)
-            {
-                if(dots[i].classList.contains("active"))
-                {
+    for(let i = 0; i < dots.length; i++) {
+        if(event.target.matches(".dot" + (i+1))) {
+            for(let i = 0; i < dots.length; i++) {
+                if(dots[i].classList.contains("active")) {
                     dots[i].classList.remove("active");
                 }
             }
             dots[i].classList.add("active");
-            bigImage.setAttribute("src", "../images/"+srcGiftCard+(i+1)+".jpg");
+            bigImage.setAttribute("src", `../images/${srcGiftCard}${i+1}.jpg`);
         }
     }
 });
